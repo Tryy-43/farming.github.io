11b0a1052ca622a11fce6ca9541a693307e4f1a8
Tryy-43 <165661078+Tryy-43@users.noreply.github.com>
2024-05-26 11:12:15 +0700

    Add files via upload

--- js/nav.js
+++ js/nav.js
@@ -1,34 +1,37 @@
-// function showSideBar(){
-//     const sidebar = document.querySelector('.sidebar');
-//     sidebar.style.display = 'flex';
+// function showSideBar() {
+//   const sidebar = document.querySelector(".sidebar");
+//   sidebar.style.display = "flex";
 // }
-// function hideSidebar(){
-//     let hidebar = document.getElementById("sidebar");
-//     hidebar.style.display = 'none';
+// function hideSidebar() {
+//   let hidebar = document.getElementById("sidebar");
+//   hidebar.style.display = "none";
 // }
-// var a;
-// function dropDownMenu(){
-//     if(a==1){
-//         document.getElementById("dropdown_menu").style.display ='flex';
-//         return a=0;
-//     }else{
-//         document.getElementById("dropdown_menu").style.display ='none';
-//         return a=1;
-//     }
+// // var a;
+// // function dropDownMenu() {
+// //   if (a == 1) {
+// //     document.getElementById("dropdown_menu").style.display = "flex";
+// //     return (a = 0);
+// //   } else {
+// //     document.getElementById("dropdown_menu").style.display = "none";
+// //     return (a = 1);
+// //   }
+// // }
+// function sidebarDown() {
+//   if (a == 1) {
+//     document.getElementById("sidebar_drop").style.display = "flex";
+//     return (a = 0);
+//   } else {
+//     document.getElementById("sidebar_drop").style.display = "none";
+//     return (a = 1);
+//   }
 // }
-// function sidebarDown(){
-//     if(a==1){
-//         document.getElementById("sidebar_drop").style.display ='flex';
-//         return a=0;
-//     }else{
-//         document.getElementById("sidebar_drop").style.display ='none';
-//         return a=1;
-//     }
-// }
-// function hidedrop(){
-//     document.getElementById("dropdown_menu").style.display = 'none';
-// }
-// function hidedrops(){
-
-//     document.getElementById("sidebar_drop").style.display = 'none';
-// }
+// // function hidedrop() {
+// //   document.getElementById("dropdown_menu").style.display = "none";
+// // }
+// function hidedrops() {
+//   document.getElementById("sidebar_drop").style.display = "none";
+// // }
+// $(document).on("click", ".hide", function () {
+//   console.log("ehell");
+//   $(".drop").slideToggle(2000);
+// });
--- js/toggle.js
+++ js/toggle.js
@@ -11,6 +11,27 @@ $(document).on("click", "#hideSidebar1", function () {
 $(document).on("click", "#sidebar_down", function () {
   $("#sidebar_drop").slideToggle(1000);
 });
+$(document).ready(function () {
+  $("#coll").slideDown(500);
+  $("#down1").toggleClass("fa-chevron-right fa-chevron-down");
+});
+$(document).on("click", "#title11", function () {
+  $("#coll").slideToggle(500);
+});
+$(document).on("click", "#title2", function () {
+  $("#coll2").slideToggle(500);
+});
+
+$(document).on("click", "#title3", function () {
+  $("#coll3").slideToggle(500);
+});
+
+// let changeIcon = function (icon) {
+//   icon.classList.toggle("fa-chevron-down");
+// };
+// $("#title3").on("click", function () {
+//   $("#down3").toggleClass();
+// });
 $("#title11").on("click", function () {
   $("#down1").toggleClass("fa-chevron-right fa-chevron-down");
 });
