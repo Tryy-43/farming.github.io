42df04b83f7d8f7026c4573942794007f714e65e
Tryy-43 <165661078+Tryy-43@users.noreply.github.com>
2024-05-26 11:07:19 +0700

    Update toggle.js

--- js/toggle.js
+++ js/toggle.js
@@ -11,3 +11,14 @@ $(document).on("click", "#hideSidebar1", function () {
 $(document).on("click", "#sidebar_down", function () {
   $("#sidebar_drop").slideToggle(1000);
 });
+$("#title11").on("click", function () {
+  $("#down1").toggleClass("fa-chevron-right fa-chevron-down");
+});
+
+$("#title2").on("click", function () {
+  $("#down2").toggleClass("fa-chevron-right fa-chevron-down");
+});
+
+$("#title3").on("click", function () {
+  $("#down3").toggleClass("fa-chevron-right fa-chevron-down");
+});
